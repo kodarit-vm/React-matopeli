1a9ca58d63317669dccaf56d4bf0c5bdd49237a8
Vekkumasa <ylirasku@gmail.com>
2021-11-09 19:28:42 +0200

    ruudunkoko input field

--- src/SnakeBoard.js
+++ src/SnakeBoard.js
@@ -4,6 +4,8 @@ import "./SnakeBoard.css";
 const SnakeBoard = ({ points, setPoints }) => {
   const [ width, setWidth ] = useState(40);
   const [ height, setHeight ] = useState(40);
+  const [ startGame, setStartGame ] = useState(false);
+  const [ error, setError ] = useState(null);
 
   const getInitialRows = () => {
     let initialRows = []
@@ -17,7 +19,37 @@ const SnakeBoard = ({ points, setPoints }) => {
   }
 
   return (
-    <div></div>
+    <div>
+      {!startGame && (
+        <div>
+          <div> Pelilaudan koko on nyt {width} ruutua. </div>
+          <div> Muuta halutessasi ruudun kokoa </div>
+          <input 
+            placeholder="Koko 10-100"
+            type="number"
+            onChange={e => {
+              const size = parseInt(e.target.value);
+              console.log('size', size);
+              if (size <= 100 && size >= 10) {
+                setWidth(size)
+                setHeight(size)
+                localStorage.setItem("snake-board-size", size)
+                setError(null)
+              } else {
+                setError(`Pelilaudan koko on liian ${size > 100 ? "suuri" : "pieni"}`)
+              }
+            }}
+          />
+          {error && <div className="Error"> {error} </div> }
+          {!error && 
+            <button onClick={() => setStartGame(!startGame)}>
+              Aloita peli
+            </button>
+          }
+          
+        </div>
+      )}
+    </div>
   )
 };
 
