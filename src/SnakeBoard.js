8b7abd13d77b09ecfb31143aeef2f1982893756a
Vekkumasa <ylirasku@gmail.com>
2021-11-16 19:28:33 +0200

    Snake-board

--- src/SnakeBoard.js
+++ src/SnakeBoard.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./SnakeBoard.css";
 
 const SnakeBoard = ({ points, setPoints }) => {
-  const [ width, setWidth ] = useState(40);
-  const [ height, setHeight ] = useState(40);
+  const [ width, setWidth ] = useState(
+    parseInt(localStorage.getItem("snake-board-size")) || 40
+  );
+  const [ height, setHeight ] = useState(
+    parseInt(localStorage.getItem("snake-board-size")) || 40
+  );
   const [ startGame, setStartGame ] = useState(false);
   const [ error, setError ] = useState(null);
 
@@ -18,8 +22,36 @@ const SnakeBoard = ({ points, setPoints }) => {
     return initialRows;
   }
 
+  const [ rows, setRows ] = useState(getInitialRows())
+
+  useEffect(() => {
+    if (width >= 10 && width <= 100 && height >= 10 && height <= 100) {
+      setRows(getInitialRows())
+    }
+  }, [width, height])
+
+  const displayRows = rows.map((row, i) => (
+    <div className="Snake-row" key={i}>
+      {row.map((tile, j) => (
+        <div className={`tile ${tile}`} key={`${i}-${j}`}/>
+      ))}
+    </div>
+  ))
+
+  const getObstacles = [
+    {
+      name: "tyhja",
+      location: []
+    },
+    {
+      name: "oma",
+      location: [{x: 6, y: 15}, {x: 1, y: 10}, {x: 5, y: 5}]
+    }
+  ]
+
+  console.log('rivit', rows);
   return (
-    <div>
+    <div className="Snake-board">
       {!startGame && (
         <div>
           <div> Pelilaudan koko on nyt {width} ruutua. </div>
@@ -45,10 +77,10 @@ const SnakeBoard = ({ points, setPoints }) => {
             <button onClick={() => setStartGame(!startGame)}>
               Aloita peli
             </button>
-          }
-          
+          }        
         </div>
       )}
+      {startGame && displayRows}
     </div>
   )
 };
